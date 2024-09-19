f6f999c144e6458a5171ab0cbb25e07de427583a
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-09-19 11:39:34 +0530

    Answer assignment_1 question_12

---
+++ Assignment_1/Question_12.py
@@ -0,0 +1,5 @@
+import math
+
+r = int(input("Enter the radius of the circle: "))
+print(f"The area of the circle with radius {r} is: {math.pi * (r**2)}")
+print(f"The volume of a sphere with radius {r} is: {(4/3) * math.pi * (r**3)}")
