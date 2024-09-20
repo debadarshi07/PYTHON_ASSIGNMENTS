859c45d970a0dd2284b7db20b69f6febf93c0079
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-09-20 11:47:53 +0530

    question_22

--- Assignment_1/Question_22.py
+++ Assignment_1/Question_22.py
@@ -1,3 +1,4 @@
 temperature_in_celsius = float(input("Enter the temperature in celcius scale: "))
 
-print(temperature_in_celsius * 9/5 + 32)
+print("In Fahrenhit:", temperature_in_celsius * 9/5 + 32)
+print("In Kelvin:", temperature_in_celsius + 273.15)
