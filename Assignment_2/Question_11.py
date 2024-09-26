ad9ea78f7606734c926e5f3a8c98e05fa537c70d
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-09-26 23:43:03 +0530

    assignment_2

---
+++ Assignment_2/Question_11.py
@@ -0,0 +1,24 @@
+import sys
+
+while True :
+    operator = input("Enter the operator(+ , - , * , /) or 'exit' to stop: ")
+    if operator == 'exit' :
+        sys.exit()
+    if operator in '+-*/':
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        match operator:
+            case '+':
+                print(f"{num1} + {num2} = {num1 + num2}")
+            case '-':
+                print(f"{num1} - {num2} = {num1 - num2}")
+            case '*':
+                print(f"{num1} * {num2} = {num1 * num2}")
+            case '/':
+                if num2 != 0:
+                    print(f"{num1} / {num2} = {num1 / num2}")
+                else :
+                    print("Error! Division by zero is not allowed.")
+    else :
+        print("Enter a valid operator")
+
---
+++ Assignment_2/Question_12.py
@@ -0,0 +1,6 @@
+import statistics as stat
+numbers = [1, 2, 3, 2, 3, 4, 4, 4, 5, 4, 5, 6]
+
+print(f"Mean of the dataset is: {stat.mean(numbers):.2f}")
+print(f"Median of the dataset is: {stat.median(numbers)}")
+print(f"Mode of the dataset is: {stat.mode(numbers)}")
