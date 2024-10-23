3324616af9827598d092b32731406c123358dc3e
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-10-23 13:50:05 +0530

    add iterable statement

--- Practice Sessions/Session_03/strings.py
+++ Practice Sessions/Session_03/strings.py
@@ -3,6 +3,7 @@ print('Full name:', name)
 
 print(name[0], name[1], name[2]) # chracters of python string can be accessed like elements of list.
 
+# python strings are iterable
 for ch in name:
     print(ch, end = ' ')
 
@@ -16,3 +17,6 @@ print('Last name:', last_name)
 
 skip_name = name[0 : 10 : 2]  # third argument is jump amount(here it's 2)
 print('Skip name:', skip_name)
+
+name[0] = 'd'
+print(name) # throws error because python strings are immutable
