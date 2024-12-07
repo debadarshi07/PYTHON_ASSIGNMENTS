cccf1c1c92d47627157951953a23bb2e050d7fb9
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-12-07 09:59:10 +0530

    Major Assignment

--- Assignments/Major_Assignment_2/Mission_4.py
+++
@@ -1,6 +0,0 @@
-# Mission - 4 (Tracking Supply Distribution)
-
-supplies = [("Kyiv", "Food", 500), ("Kursk", "Medicines", 200), ("Lviv", "Water", 300), ("Chelyabinsk", "Blankets", 100), ("Kharkiv", "Food", 150), ("Luhansk", "Tents", 600), ("Vladivostok", "Water", 400)]
-
-supply_details = {details[0]: {details[1]: details[2]} for details in supplies}
-print(f"Supply details: \n{supply_details}")
