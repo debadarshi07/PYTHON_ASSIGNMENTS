bb8907ee548d2e0220b30004a6f097b24faab2a5
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-01-11 12:56:29 +0530

    conditionals in python

--- Practice Sessions/Session_06/conditionals.py
+++ Practice Sessions/Session_06/conditionals.py
@@ -5,3 +5,22 @@ elif age < 18 and age > 0:
     print('You are not eligible to cast your vote.\n')
 else:
     print('Age can not be negative. Enter valid age.\n')
+
+day = input('Enter the day of the week: ')
+match day:
+    case 'Monday':
+        print('Today is Monday.')
+    case 'Tuesday':
+        print('Today is Tuesday.')
+    case 'Wednesday':
+        print('Today is Wednesday.')
+    case 'Thursday':
+        print('Today is Thursday.')
+    case 'Friday':
+        print('Today is Friday.')
+    case 'Saturday':
+        print('Today is Saturday.')
+    case 'Sunday':
+        print('Today is Sunday.')
+    case _:
+        print('Invalid day entered.')
